a07e5f5071c131a1ea908bc6f3a996fc324fc40e
jspiner <jspiner@naver.com>
2018-07-08 17:23:56 +0900

    StaggledGridLayout으로 레이아웃 변경

--- app/src/main/java/net/jspiner/cherrypick/ui/main/recipe/RecipeFragment.java
+++ app/src/main/java/net/jspiner/cherrypick/ui/main/recipe/RecipeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
 import net.jspiner.cherrypick.R;
@@ -50,7 +51,9 @@ public class RecipeFragment extends BaseFragment<FragmentRecipeBinding, Contract
 
     private void initRecyclerView() {
         recipeAdapter = new RecipeAdapter();
-        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
         binding.recyclerView.setAdapter(recipeAdapter);
 
         addDummyData();
