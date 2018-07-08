c022e5b21bb5d4de57b42007e2d9502069d91368
jspiner <jspiner@naver.com>
2018-07-08 17:24:12 +0900

    테스트용 이미지를 viewholder에서 로딩

--- app/src/main/java/net/jspiner/cherrypick/ui/main/recipe/RecipeViewHolder.java
+++ app/src/main/java/net/jspiner/cherrypick/ui/main/recipe/RecipeViewHolder.java
@@ -2,6 +2,8 @@ package net.jspiner.cherrypick.ui.main.recipe;
 
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+
 import net.jspiner.cherrypick.databinding.CardRecipeBinding;
 import net.jspiner.cherrypick.model.Recipe;
 import net.jspiner.cherrypick.ui.base.BaseViewHolder;
@@ -14,6 +16,8 @@ public class RecipeViewHolder extends BaseViewHolder<CardRecipeBinding, Recipe> 
 
     @Override
     public void setData(Recipe recipe) {
-
+        Glide.with(binding.getRoot().getContext())
+                .load("https://cdn.liquor.com/wp-content/uploads/2013/03/04113537/dry-martini-7200-720-recipe.jpg")
+                .into(binding.image);
     }
 }
