d2e2546c93782e826cbb28179cbd875bfeb65c04
jspiner <jspiner@naver.com>
2018-07-12 21:35:34 +0900

    viewholder에서 마지막으로 set된 데이터를 저장하는 변수 추가

--- app/src/main/java/net/jspiner/cherrypick/ui/base/BaseViewHolder.java
+++ app/src/main/java/net/jspiner/cherrypick/ui/base/BaseViewHolder.java
@@ -2,6 +2,7 @@ package net.jspiner.cherrypick.ui.base;
 
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
+import android.support.annotation.CallSuper;
 import android.support.annotation.LayoutRes;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,12 +11,16 @@ import android.view.ViewGroup;
 public abstract class BaseViewHolder<B extends ViewDataBinding, D> extends RecyclerView.ViewHolder {
 
     protected B binding;
+    protected D lastData;
 
     public BaseViewHolder(ViewGroup parent, @LayoutRes int layoutResId) {
         super(LayoutInflater.from(parent.getContext()).inflate(layoutResId, parent, false));
         this.binding = DataBindingUtil.bind(itemView);
     }
 
-    public abstract void setData(D data);
+    @CallSuper
+    public void setData(D data) {
+        lastData = data;
+    }
 
 }
