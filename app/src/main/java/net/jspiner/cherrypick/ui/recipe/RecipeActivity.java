f7fdb4b5c286ca6fcbe925cddf03d4c14d61db3e
jspiner <jspiner@naver.com>
2018-07-12 21:34:12 +0900

    Recipe activity에 Recipe 객체를 전달할수 있도록 intent 추가

--- app/src/main/java/net/jspiner/cherrypick/ui/recipe/RecipeActivity.java
+++ app/src/main/java/net/jspiner/cherrypick/ui/recipe/RecipeActivity.java
@@ -1,11 +1,29 @@
 package net.jspiner.cherrypick.ui.recipe;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.gson.Gson;
+
 import net.jspiner.cherrypick.R;
 import net.jspiner.cherrypick.databinding.ActivityRecipeBinding;
+import net.jspiner.cherrypick.model.Recipe;
 import net.jspiner.cherrypick.ui.base.BaseActivity;
+import net.jspiner.cherrypick.ui.util.IntentKey;
 
 public class RecipeActivity extends BaseActivity<ActivityRecipeBinding, Contract.Presenter>
         implements Contract.View {
+
+    private Recipe recipe;
+
+    public static void startActivity(Context context, @NonNull Recipe recipe) {
+        Intent intent = new Intent(context, RecipeActivity.class);
+        intent.putExtra(IntentKey.INTENT_KEY_RECIPE, new Gson().toJson(recipe));
+        context.startActivity(intent);
+    }
 
     @Override
     protected int getLayoutId() {
@@ -15,6 +33,20 @@ public class RecipeActivity extends BaseActivity<ActivityRecipeBinding, Contract
     @Override
     protected Contract.Presenter createPresenter() {
         return new RecipePresenter(this);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        init();
+    }
+
+    private void init() {
+        recipe = new Gson().fromJson(
+                getIntent().getStringExtra(IntentKey.INTENT_KEY_RECIPE),
+                Recipe.class
+        );
     }
 
 }
---
+++ app/src/main/java/net/jspiner/cherrypick/ui/util/IntentKey.java
@@ -0,0 +1,7 @@
+package net.jspiner.cherrypick.ui.util;
+
+public final class IntentKey {
+    
+    public static final String INTENT_KEY_RECIPE = "intentKeyRecipe";
+    
+}
